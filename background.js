0ea61dba92cb48847e3eeb5286583066043fcd0f
James Jezewski <james.r.jezewski@gmail.com>
2019-03-10 18:14:09 -0400

    add firebase credentials

--- background.js
+++ background.js
@@ -1,9 +1,13 @@
 // TODO(DEVELOPER): Change the values below using values from the initialization snippet: Firebase Console > Overview > Add Firebase to your web app.
 // Initialize Firebase
 var config = {
-  apiKey: '<YOUR_API_KEY>',
-  databaseURL: '<YOUR_DATABASE_URL>',
-  storageBucket: '<YOUR_STORAGE_BUCKET_NAME>'
+  apiKey: "AIzaSyCovi9ZhNIkRH_EQy5pWBhQP08jUmHDh5A",
+  authDomain: "plug-e90dc.firebaseapp.com",
+  databaseURL: "https://plug-e90dc.firebaseio.com",
+  projectId: "plug-e90dc",
+  storageBucket: "plug-e90dc.appspot.com",
+  messagingSenderId: "680708701463"
+
 };
 firebase.initializeApp(config);
 
