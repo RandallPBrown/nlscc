c938dce03599e51bf462ea949afc80c424336e89
James Jezewski <james.r.jezewski@gmail.com>
2019-03-10 20:10:40 -0400

    added firebase config file

---
+++ firebase.config.js
@@ -0,0 +1,11 @@
+// TODO(DEVELOPER): Change the values below using values from the initialization snippet: Firebase Console > Overview > Add Firebase to your web app.
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCovi9ZhNIkRH_EQy5pWBhQP08jUmHDh5A",
+    authDomain: "plug-e90dc.firebaseapp.com",
+    databaseURL: "https://plug-e90dc.firebaseio.com",
+    projectId: "plug-e90dc",
+    storageBucket: "plug-e90dc.appspot.com",
+    messagingSenderId: "680708701463"
+  
+  };
