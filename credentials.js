f5a5e7537bad48780f0075e01da1429fae4fefd7
James Jezewski <james.r.jezewski@gmail.com>
2019-03-11 18:11:03 -0400

    cleaned up credentials and main files

--- credentials.js
+++ credentials.js
@@ -5,37 +5,26 @@ var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
       // return true;
       UIkit.modal('#modal-sections').hide();
     },
     uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
-      document.getElementById('loader').style.display = 'none';
     }
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  // signInSuccessUrl: '<url-to-redirect-to-on-success>',
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
+  ]
   // Terms of service url.
-  tosUrl: '<your-tos-url>',
+  // tosUrl: '<your-tos-url>',
   // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
+  // privacyPolicyUrl: '<your-privacy-policy-url>'
 };
 
 // The start method will wait until the DOM is loaded.
-
-
-
 function initApp() {
-  // Listen for auth state changes.
-  // [START authstatelistener]
   firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
       // User is signed in.
@@ -46,68 +35,21 @@ function initApp() {
       var isAnonymous = user.isAnonymous;
       var uid = user.uid;
       var providerData = user.providerData;
-      // [START_EXCLUDE]
-      document.getElementById('quickstart-button').textContent = 'Sign out';
       document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-      document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-      // [END_EXCLUDE]
     } else {
-      // Let's try to get a Google auth token programmatically.
-      // [START_EXCLUDE]
-
       ui.start('#firebaseui-auth-container', uiConfig);
-
-
-      document.getElementById('quickstart-button').textContent = 'Sign-in with Google';
-      document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-      document.getElementById('quickstart-account-details').textContent = 'null';
       UIkit.modal('#modal-sections').show();
-      // [END_EXCLUDE]
-    }
-    document.getElementById('quickstart-button').disabled = false;
-  });
-  // [END authstatelistener]
-
-  document.getElementById('quickstart-button').addEventListener('click', startSignIn, false);
-}
-
-/**
- * Start the auth flow and authorizes to Firebase.
- * @param{boolean} interactive True if the OAuth flow should request with an interactive mode.
- */
-function startAuth(interactive) {
-  // Request an OAuth token from the Chrome Identity API.
-  chrome.identity.getAuthToken({interactive: !!interactive}, function(token) {
-    if (chrome.runtime.lastError && !interactive) {
-      console.log('It was not possible to get a token programmatically.');
-    } else if(chrome.runtime.lastError) {
-      console.error(chrome.runtime.lastError);
-    } else if (token) {
-      // Authorize Firebase with the OAuth Access Token.
-      var credential = firebase.auth.GoogleAuthProvider.credential(null, token);
-      firebase.auth().signInAndRetrieveDataWithCredential(credential).catch(function(error) {
-        // The OAuth token might have been invalidated. Lets' remove it from cache.
-        if (error.code === 'auth/invalid-credential') {
-          chrome.identity.removeCachedAuthToken({token: token}, function() {
-            startAuth(interactive);
-          });
-        }
-      });
-    } else {
-      console.error('The OAuth Token was null');
     }
   });
+  document.getElementById('logout-button').addEventListener('click', logout, false);
 }
 
 /**
  * Starts the sign-in process.
  */
-function startSignIn() {
-  document.getElementById('quickstart-button').disabled = true;
+function logout() {
   if (firebase.auth().currentUser) {
     firebase.auth().signOut();
-  } else {
-    startAuth(true);
   }
 }
 
