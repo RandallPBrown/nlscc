57c4eb24509826b05bb294615f4f337465677415
James Jezewski <james.r.jezewski@gmail.com>
2019-03-10 21:18:09 -0400

    Cleaned up comments

--- credentials.js
+++ credentials.js
@@ -1,19 +1,5 @@
 firebase.initializeApp(config);
 
-/**
- * initApp handles setting up the Firebase context and registering
- * callbacks for the auth status.
- *
- * The core initialization is in firebase.App - this is the glue class
- * which stores configuration. We provide an app name here to allow
- * distinguishing multiple app instances.
- *
- * This method also registers a listener with firebase.auth().onAuthStateChanged.
- * This listener is called when the user is signed in or out, and that
- * is where we update the UI.
- *
- * When signed in, we also authenticate to the Firebase Realtime Database.
- */
 function initApp() {
   // Listen for auth state changes.
   // [START authstatelistener]
